4b9f06832e0db4d26ce521c8d0cc5681504a16d4
Eder Barrios <ederb1.1c@gmail.com>
2019-12-15 12:45:02 -0500

    readding yaml config and args are read  by terminal

---
+++ main.py
@@ -0,0 +1,18 @@
+import argparse
+from common import config
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+def _news_scraper(key):
+    host = config()e["news_sites"][key]['url']
+    print(host)
+    logging.info(f'scrap to %s',(key))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    new_sites_choices = list(config()['news_sites'].keys())
+    parser.add_argument("news_site",help="the new site that you want to scrape",type=str,choices=new_sites_choices)
+    args = parser.parse_args()
+    _news_scraper(args.news_site)
+
